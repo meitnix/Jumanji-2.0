f04809fe9f5132ea9f482e6b99a23473cdc87f35
meitnix <55987634+meitnix@users.noreply.github.com>
2023-07-05 09:25:31 +0400

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -41,7 +41,7 @@ const login = async (req, res) => {
     // Secure cookie with refresh token 
     res.cookie('jwt', refreshToken, {httpOnly: true, secure: true, sameSite: 'None', maxAge: 7 * 24 * 60 * 60 * 1000 })
     
-    res.json({ accessToken })
+    res.json({ accessToken,message: 'Success'})
 }
 
 // @desc Refresh
